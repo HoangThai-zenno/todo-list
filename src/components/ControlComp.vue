d20bc10d4dfb47ebcf8d27eedc7fa06069e75fc7
Hoang_Thai <tlee692001@gmail.com>
2023-05-09 12:54:17 +0700

    Create Components
    separate the UI to components

---
+++ src/components/ControlComp.vue
@@ -0,0 +1,27 @@
+<template>
+	<b-col cols="12" lg="6">
+		<b-row>
+			<!-- SORT : START -->
+			<control-sort />
+			<!-- SORT : END -->
+
+			<!-- SEARCH : START -->
+			<control-search />
+			<!-- SEARCH : END -->
+		</b-row>
+	</b-col>
+</template>
+
+<script>
+import ControlSearch from './ControlSearch.vue';
+import ControlSort from './ControlSort.vue';
+export default {
+	name: 'control-comp',
+	components: {
+		ControlSearch,
+		ControlSort
+	}
+}
+</script>
+
+<style></style>
