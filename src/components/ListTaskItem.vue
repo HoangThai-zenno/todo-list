6ea3e0e4ea0b179d7dd88e92adf23b4a4ec21ddc
Hoang_Thai <tlee692001@gmail.com>
2023-05-09 21:15:00 +0700

    Mock Data

--- src/components/ListTaskItem.vue
+++ src/components/ListTaskItem.vue
@@ -1,8 +1,8 @@
 <template>
     <tr>
-        <td class="text-center">1</td>
-        <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
-        <td class="text-center"><span class="badge badge-danger">High</span></td>
+        <td class="text-center">{{ index }}</td>
+        <td>{{ list.taskName }}</td>
+        <td class="text-center"><span class="badge" v-bind:class="classLevel">{{ getLevelName }}</span></td>
         <td>
             <button type="button" class="btn btn-warning">Edit</button>
             <button type="button" class="btn btn-danger">Delete</button>
@@ -12,7 +12,38 @@
 
 <script>
 export default {
-    name: 'list-task-item'
+    name: 'list-task-item',
+    props: {
+        list: { type: Object, defaultValue: null },
+        index: { type: Number }
+    },
+    computed: {
+        getLevelName() {
+            switch (this.list.level) {
+                case 0:
+                    return 'Small';
+                case 1:
+                    return 'Medium';
+                case 2:
+                    return 'Hight';
+                default:
+                    return ''
+            }
+        },
+        classLevel(){
+            switch (this.list.level) {
+                case 0:
+                    return {'badge-info': true};
+                case 1:
+                    return {'badge-warning': true};
+                case 2:
+                    return {'badge-danger': true};
+                default:
+                    return ''
+            }
+
+        }
+    }
 }
 </script>
 
